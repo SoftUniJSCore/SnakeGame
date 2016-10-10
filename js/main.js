db0d838f7659a74ff4b82b6c1f0429da9c1e2064
Gabriela <gabi.ivanova@gmail.com>
2016-10-10 13:33:51 +0300

    game over msg update

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ var mainLogic = (function () {
       var modal = document.getElementById('myModal');
 
       // Get the button that opens the modal
-      var modalContent = document.getElementById("modalText").innerHTML = "Score: " + score + "<br />" + "Game Over";
+      var modalContent = document.getElementById("modalText").innerHTML = "Game Over <br />" +  "Score: " + score ;
 
 
       // Get the <span> element that closes the modal

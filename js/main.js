0127e7f8aff489b6599e96cef0080ab16fa3e83f
Panayot Kindalov <panayotkindalov@gmail.com>
2016-10-12 21:54:59 +0300

    Reset information after game over

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ var mainLogic = (function () {
 	var tenPointsFood = 0;
 	var pointsWithoutBonuses = 0;
 	var foodBonusScore = 0;
+	
 
     var drawSnake = function (x, y) {
         ctx.fillStyle = 'green';
@@ -119,6 +120,11 @@ var mainLogic = (function () {
 //game over - snakeSize - snake width - calc how many sqares it have
         if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || checkCollision(snakeX, snakeY, snake)) {
             //restart game - active start button
+			onePointFood = 0;
+			fivePointFood = 0;
+			tenPointsFood = 0;
+			pointsWithoutBonuses = 0;
+			foodBonusScore = 0;
             btn.removeAttribute('disabled', true);
             pause_btn.setAttribute('disabled', true)
 
